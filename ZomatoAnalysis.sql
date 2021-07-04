ce0ccfe9564f067cc12eb1b05efc2bcc81349df1
Monika Singh <83471033+monikasingh1109@users.noreply.github.com>
2021-07-04 15:30:13 +0530

    Update ZomatoAnalysis.sql

--- ZomatoAnalysis.sql
+++ ZomatoAnalysis.sql
@@ -85,16 +85,67 @@ group by ct.Country
 order by Avg_Rate desc
 
 
+--Cities with poor restaurant rating
+select distinct(City),AVG(Rating) as Average_rating
+from Zomato
+group by City
+having AVG(Rating)<3
+order by Average_rating
 
+--Restaurants in Kolkata which provides Online delivery
+select RestaurantName,Rating
+from Zomato
+where City='Kolkata' and Has_Online_delivery='Yes'
 
-
-
-
-select * from Zomato
-
+--Pizza restaurants in New delhi with average rating 3+
+select RestaurantName
+from Zomato
+where City='New Delhi' and Cuisines='Pizza' and Rating>3
 
 select distinct(country)
 from [dbo].[CountryTable] 
  
+ select * from Zomato
+ --Most affordable and High rated restaunrant in city
 
- --
+ select RestaurantName,Average_Cost_for_two
+ from Zomato
+ 
+
+ --restaurants with poor offline service
+
+ select Avg(Rating) as Avg_Rate,RestaurantName,City
+ from Zomato
+ where Has_Online_delivery='No' and Rating<3
+ group by City,RestaurantName
+ order by Avg_Rate desc
+
+ --cities with atleast 3 restaurants with rating >=4.5 and in case there are two cities with same count,sort them alphabetically
+ select City,count(RestaurantName) as RestCount,AVG(Rating) as avg_rate
+ from Zomato
+ where Rating>=4.5
+ group by City
+ having COUNT(RestaurantName)>3
+ order by City asc
+
+
+ --Average cost of 2 across all zomato linked restaurants
+ 
+ select avg(Average_Cost_for_two)
+ from Zomato
+
+ select * from Zomato
+--Top 5 restaurants with highest rating
+select top 5 RestaurantName,count(RestaurantName) as Count_Res,max(Rating) as Avg_Rate
+from Zomato
+group by RestaurantName
+order by Avg_Rate desc
+
+-- top 5 restaurants with max votes
+select top 5 RestaurantName,count(RestaurantName),MAX(Votes) as maximum_Votes
+from Zomato
+group by RestaurantName
+order by maximum_Votes desc
+
+--Group restaurants basis on cost for two - Luxurious Expensive,Very Expensive,Expensive,High,Medium High,Average
+--Find number of restaurants for in each category
